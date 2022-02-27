405b4f29bea19c5aa3ffd2d7dde6dc7705462fce
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2022-02-27 19:57:07 +0600

    new

--- website/shop.py
+++ website/shop.py
@@ -1,16 +1,23 @@
 from flask import Blueprint,render_template
+from .__init__ import db
 
 
 shop=Blueprint('shop', __name__)
 
 @shop.route('/shop')
 def interior_shop():
-    return render_template('shop/shop.html')
+    cur=db.connection.cursor()
+    cur.execute("SELECT * FROM products")
+    products=cur.fetchall()
+    return render_template('shop/shop.html',products=products)
 
 
-@shop.route('/detail')
-def detail():
-    return render_template("shop/detail.html")
+@shop.route('/detail/<int:id>')
+def detail(id):
+    cur=db.connection.cursor()
+    cur.execute("SELECT * FROM products WHERE product_id=%s",(id,))
+    single_product=cur.fetchone()
+    return render_template("shop/detail.html", single_product=single_product)
 
 
 @shop.route('/checkout')
